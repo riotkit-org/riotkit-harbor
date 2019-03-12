10207e6470facfc07249929137a5889cf0c2cc7f
Andrew Johnson <blackandred@users.noreply.github.com>
2019-03-12 07:44:17 +0100

    The environment should now get up earlier after `make start`

--- hooks.d/post-start/ensure-gateway-is-up.sh
+++ hooks.d/post-start/ensure-gateway-is-up.sh
@@ -2,9 +2,9 @@
 CONTAINER_NAME=${COMPOSE_PROJECT_NAME}_gateway_proxy_gen_1
 
 sudo docker start ${CONTAINER_NAME}
-sleep 10
+sleep 5
 
-# retry
-if ! sudo docker ps |grep ${CONTAINER_NAME} > /dev/null; then
+while ! sudo docker ps |grep ${CONTAINER_NAME} > /dev/null; do
     sudo docker start ${CONTAINER_NAME}
-fi
+    sleep 1
+done
