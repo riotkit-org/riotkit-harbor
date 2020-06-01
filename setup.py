607b92925d6cf7c0ba934249069338a27d883f71
Andrew Johnson <blackandred@users.noreply.github.com>
2020-06-01 23:31:18 +0200

    Custom binary "harbor"

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True
+)
--- src/harbor/__init__.py
+++ src/harbor/__init__.py
@@ -1,4 +1,8 @@
+import os
 from rkd.syntax import TaskDeclaration
+from rkd import main as rkd_main
+from rkd.standardlib.env import GetEnvTask
+from rkd.standardlib.env import SetEnvTask
 from .tasks.running import ListContainersTask
 from .tasks.running import StartTask
 from .tasks.running import StopTask
@@ -15,19 +19,17 @@ from .tasks.gateway import ForceReloadSSLTask
 from .tasks.configsmanagement import ListConfigsTask
 from .tasks.configsmanagement import EnableConfigTask
 from .tasks.configsmanagement import DisableConfigTask
-from .tasks.env import GetEnvTask
-from .tasks.env import SetEnvTask
 
 
 def imports():
     return [
         TaskDeclaration(ListContainersTask()),
         TaskDeclaration(StartTask()),
+        TaskDeclaration(StopTask()),
         TaskDeclaration(ListDefinedServices()),
         TaskDeclaration(ServiceUpTask()),
         TaskDeclaration(ServiceDownTask()),
         TaskDeclaration(ServiceRemoveTask()),
-        TaskDeclaration(StopTask()),
         TaskDeclaration(StopAndRemoveTask()),
         TaskDeclaration(PullTask()),
         TaskDeclaration(RestartTask()),
@@ -40,3 +42,13 @@ def imports():
         TaskDeclaration(GetEnvTask()),
         TaskDeclaration(SetEnvTask())
     ]
+
+
+def main():
+    os.environ['RKD_WHITELIST_GROUPS'] = ':env,:harbor,'
+    os.environ['RKD_ALIAS_GROUPS'] = '->:harbor'
+    rkd_main()
+
+
+if __name__ == '__main__':
+    main()
