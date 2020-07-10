fa5d47595fa7e9a122ed8f5099705cfefe183198
Andrew Johnson <blackandred@users.noreply.github.com>
2020-07-10 08:21:40 +0200

    #9: Publish to PyPI

--- .rkd/makefile.py
+++ .rkd/makefile.py
@@ -6,7 +6,7 @@ from rkd_python import imports as PythonImports
 IMPORTS = [] + PythonImports() + HarborImports()
 
 TASKS = [
-    Task(':release', description='Release RKD to PyPI (snapshot when on master, release on tag)',
+    Task(':release', description='Release to PyPI (snapshot when on master, release on tag)',
          to_execute=[
             ':py:build', ':py:publish', '--username=__token__', '--password=${PYPI_TOKEN}'
          ]),
